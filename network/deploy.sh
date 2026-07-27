ce4851fe071b66d2890822199e65ffdb39ec69f5
fahdaguenouz <faguenouz@gmail.com>
2026-07-27 21:35:45 +0100

    updating docker

--- network/deploy.sh
+++ network/deploy.sh
@@ -6,15 +6,18 @@ export FABRIC_CFG_PATH=${PWD}
 
 echo "Stopping and removing existing containers..."
 docker-compose down -v
-rm -rf crypto-config system-genesis-block organizations/fabric-ca/org1/* organizations/fabric-ca/org2/*
+sudo rm -rf crypto-config channel-artifacts organizations/fabric-ca/org1/* organizations/fabric-ca/org2/*
 
-mkdir -p system-genesis-block organizations/fabric-ca/org1 organizations/fabric-ca/org2
+mkdir -p channel-artifacts organizations/fabric-ca/org1 organizations/fabric-ca/org2
 
 echo "Generating crypto material..."
 cryptogen generate --config=./crypto-config.yaml --output="crypto-config"
 
-echo "Generating channel genesis block..."
-configtxgen -profile PostalServicesChannel -outputBlock ./system-genesis-block/genesis.block -channelID postalservices
+echo "Generating system genesis block and channel transaction..."
+# 1. Generate System Channel Block for the Orderer
+configtxgen -profile OrdererGenesis -outputBlock ./channel-artifacts/genesis.block -channelID system-channel
+# 2. Generate Application Channel Transaction for the Peers
+configtxgen -profile PostalServicesChannel -outputCreateChannelTx ./channel-artifacts/postalservices.tx -channelID postalservices
 
 echo "Starting network..."
 docker-compose up -d
@@ -22,7 +25,7 @@ docker-compose up -d
 echo "Waiting for network to start..."
 sleep 10
 
-echo "Joining peers to the channel..."
+echo "Creating and Joining channel..."
 docker exec cli bash -c "./scripts/joinChannel.sh"
 
 echo "Deploying chaincode..."
--- network/scripts/joinChannel.sh
+++ network/scripts/joinChannel.sh
@@ -3,16 +3,18 @@
 . scripts/envVar.sh
 
 CHANNEL_NAME="postalservices"
-GENESIS_BLOCK="./channel-artifacts/genesis.block"
+
+echo "Creating channel on the Orderer..."
+setGlobals 1 0
+peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/postalservices.tx --outputBlock ./channel-artifacts/${CHANNEL_NAME}.block --tls --cafile $ORDERER_CA
 
 echo "Joining peer0.org1 (Nairobi) to the channel..."
-setGlobals 1 0
-peer channel join -b $GENESIS_BLOCK
+peer channel join -b ./channel-artifacts/${CHANNEL_NAME}.block
 
 echo "Joining peer1.org1 (Atlanta) to the channel..."
 setGlobals 1 1
-peer channel join -b $GENESIS_BLOCK
+peer channel join -b ./channel-artifacts/${CHANNEL_NAME}.block
 
 echo "Joining peer0.org2 (Singapore) to the channel..."
 setGlobals 2 0
-peer channel join -b $GENESIS_BLOCK
+peer channel join -b ./channel-artifacts/${CHANNEL_NAME}.block
